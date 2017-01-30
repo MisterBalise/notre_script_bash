4d98ec5658e58a937252ddaa0c82ccd4f4a0a3b4
MisterBalise <emmanuelkpomahou@gmail.com>
2017-01-30 13:58:13 -0500

    Ajout de la boucle for permettant de parcourir chaque lettre du fichier

--- script.sh
+++ script.sh
@@ -8,3 +8,10 @@ then
 	# On vide le fichier stats.txt (ou on le crée s'il n'existe pas déjà)
 	echo '' > stats.txt
 
+# Pour chaque lettre de l'alphabet, on compte le nombre d'occurrences et on l'enregistre dans stats.txt
+for lettre in {A..Z}
+do
+	    occurrences=`grep $lettre $1 | wc -l`
+	        echo "$occurrences - $lettre" >> stats.txt
+	done
+
